226f50deecfe2e6dfef3b31e788e16e78f712a13
rellika <rellika@lwalacommunityalliance.org>
2022-10-16 23:44:04 +0300

    ADDED INPUTBOX.JS

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -32,6 +32,15 @@ const styles = StyleSheet.create({
 		padding: 10,
 		borderRadius: 10,
 		maxWidth: '80%',
+		shadowColor: '#000',
+		shadowOffset: {
+			width: 0,
+			height: 1,
+		},
+		shadowOpacity: 0.2,
+		shadowRadius: 1.41,
+
+		elevation: 2,
 	},
 	message: {},
 	time: {
--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -1,18 +1,30 @@
-import { ImageBackground, StyleSheet, FlatList } from 'react-native'
+import {
+	ImageBackground,
+	StyleSheet,
+	FlatList,
+	KeyboardAvoidingView,
+} from 'react-native'
 import bg from '../../assets/images/BG.png'
 import Message from '../components/Message'
 import messages from '../../assets/data/messages.json'
+import InputBox from '../components/InputBox'
 
 const ChatScreen = () => {
 	return (
-		<ImageBackground source={bg} style={styles.bg}>
-			<FlatList
-				data={messages}
-				renderItem={({ item }) => <Message message={item} />}
-				style={{ padding: 10 }}
-				inverted
-			/>
-		</ImageBackground>
+		<KeyboardAvoidingView
+			behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+			style={styles.bg}
+		>
+			<ImageBackground source={bg} style={styles.bg}>
+				<FlatList
+					data={messages}
+					renderItem={({ item }) => <Message message={item} />}
+					style={{ padding: 10 }}
+					inverted
+				/>
+				<InputBox />
+			</ImageBackground>
+		</KeyboardAvoidingView>
 	)
 }
 
