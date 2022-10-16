2be12cf4315c9f302420ecbca7803d4d648c6892
rellika <rellika@lwalacommunityalliance.org>
2022-10-16 23:14:19 +0300

    ADDED DUMMY DATA FOR CHATS

--- src/components/Message/index.js
+++ src/components/Message/index.js
@@ -1,11 +1,43 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+dayjs.extend(relativeTime)
 
 const Message = ({ message }) => {
+	const isMyMessage = () => {
+		return message.user.id === 'u1'
+	}
+
 	return (
-		<View>
+		<View
+			style={[
+				styles.container,
+				{
+					backgroundColor: isMyMessage() ? '#DCF8C5' : 'white',
+					alignSelf: isMyMessage() ? 'flex-end' : 'flex-start',
+				},
+			]}
+		>
 			<Text>{message.text}</Text>
+			<Text style={styles.time}>
+				{dayjs(message.createdAt).fromNow(true)}
+			</Text>
 		</View>
 	)
 }
+
+const styles = StyleSheet.create({
+	container: {
+		margin: 5,
+		padding: 10,
+		borderRadius: 10,
+		maxWidth: '80%',
+	},
+	message: {},
+	time: {
+		alignSelf: 'flex-end',
+		color: 'grey',
+	},
+})
 
 export default Message
