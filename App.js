f92fce2f8b69503d7d177a1bc21b7228f0318211
rellika <rellika@lwalacommunityalliance.org>
2022-10-16 23:13:00 +0300

    CREATED MESSAGE COMPONENT AND CHATSCREEN

--- App.js
+++ App.js
@@ -1,24 +1,12 @@
-import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
-import ChatListItem from './src/components/ChatListItem'
-import ChatsScreen from './src/screens/ChatsScreen'
+// import ChatsScreen from './src/screens/ChatsScreen'
+import ChatScreen from './src/screens/ChatScreen'
 
 export default function App() {
-	const chat = {
-		id: '1',
-		user: {
-			image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg',
-			name: 'Lukas',
-		},
-		lastMessage: {
-			text: 'Hi there. How are you doing. Just wanted to check on theprogress of the project? How is the design coming up?',
-			createdAt: '07:30',
-		},
-	}
-
 	return (
 		<View style={styles.container}>
-			<ChatsScreen />
+			{/* <ChatsScreen /> */}
+			<ChatScreen />
 		</View>
 	)
 }
