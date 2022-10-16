8e1515d06e56c1c221bb7ba874d21c5c9e3e335c
rellika <rellika@lwalacommunityalliance.org>
2022-10-17 00:11:43 +0300

    PASSED NAVIGATION PARAMS

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -8,8 +8,19 @@ import bg from '../../assets/images/BG.png'
 import Message from '../components/Message'
 import messages from '../../assets/data/messages.json'
 import InputBox from '../components/InputBox'
+import { useRoute, useNavigation } from '@react-navigation/native'
+import { useEffect } from 'react'
 
 const ChatScreen = () => {
+	const navigation = useNavigation()
+	const route = useRoute()
+
+	console.log(route)
+
+	useEffect(() => {
+		navigation.setOptions({ title: route.params.name })
+	}, [route.params])
+
 	return (
 		<KeyboardAvoidingView
 			behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
