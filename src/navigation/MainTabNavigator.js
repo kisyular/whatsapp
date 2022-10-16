209def1b186042c93348ffa8ca18ddd86c924c49
rellika <rellika@lwalacommunityalliance.org>
2022-10-17 00:38:40 +0300

    ADDED MAIN TAB NAVIGATOR - BOTTOM NAVIGATION

---
+++ src/navigation/MainTabNavigator.js
@@ -0,0 +1,93 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import ChatsScreen from '../screens/ChatsScreen'
+import NotImplementedScreen from '../screens/NotImplementedScreen'
+import { Ionicons, Entypo } from '@expo/vector-icons'
+const Tab = createBottomTabNavigator()
+
+const MainTabNavigator = () => {
+	return (
+		<Tab.Navigator
+			initialRouteName='Chats'
+			screenOptions={{
+				tabBarStyle: { backgroundColor: 'whitesmoke' },
+				headerStyle: { backgroundColor: 'whitesmoke' },
+			}}
+		>
+			<Tab.Screen
+				name='Status'
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<Ionicons
+							name='logo-whatsapp'
+							size={size}
+							color={color}
+						/>
+					),
+				}}
+				component={NotImplementedScreen}
+			/>
+			<Tab.Screen
+				name='Calls'
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<Ionicons
+							name='call-outline'
+							size={size}
+							color={color}
+						/>
+					),
+				}}
+				component={NotImplementedScreen}
+			/>
+			<Tab.Screen
+				name='Camera'
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<Ionicons
+							name='camera-outline'
+							size={size}
+							color={color}
+						/>
+					),
+				}}
+				component={NotImplementedScreen}
+			/>
+			<Tab.Screen
+				name='Chats'
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<Ionicons
+							name='ios-chatbubbles-sharp'
+							size={size}
+							color={color}
+						/>
+					),
+					headerRight: () => (
+						<Entypo
+							name='new-message'
+							size={18}
+							color={'royalblue'}
+							style={{ marginRight: 15 }}
+						/>
+					),
+				}}
+				component={ChatsScreen}
+			/>
+			<Tab.Screen
+				name='Settings'
+				options={{
+					tabBarIcon: ({ color, size }) => (
+						<Ionicons
+							name='settings-outline'
+							size={size}
+							color={color}
+						/>
+					),
+				}}
+				component={NotImplementedScreen}
+			/>
+		</Tab.Navigator>
+	)
+}
+
+export default MainTabNavigator
--- src/navigation/index.js
+++ src/navigation/index.js
@@ -1,16 +1,25 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-
-import ChatsScreen from '../screens/ChatsScreen'
+// import ChatsScreen from '../screens/ChatsScreen'
 import ChatScreen from '../screens/ChatScreen'
+import MainTabNavigator from './MainTabNavigator'
 
 const Stack = createNativeStackNavigator()
 
 const Navigator = () => {
 	return (
 		<NavigationContainer>
-			<Stack.Navigator>
-				<Stack.Screen name='Chats' component={ChatsScreen} />
+			<Stack.Navigator
+				screenOptions={{
+					headerStyle: { backgroundColor: 'whitesmoke' },
+				}}
+			>
+				<Stack.Screen
+					name='Main'
+					component={MainTabNavigator}
+					options={{ headerShown: false }}
+				/>
+				{/* <Stack.Screen name='Chats' component={ChatsScreen} /> */}
 				<Stack.Screen name='Chat' component={ChatScreen} />
 			</Stack.Navigator>
 		</NavigationContainer>
