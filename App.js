ebec10645492e9fdc9d156ac0131139b0835aeff
rellika <rellika@lwalacommunityalliance.org>
2022-10-16 20:24:26 +0300

    ADDED CHATLISTITEM.JS WITH PROPS

--- App.js
+++ App.js
@@ -3,10 +3,22 @@ import { StyleSheet, Text, View } from 'react-native'
 import ChatListItem from './src/components/ChatListItem'
 
 export default function App() {
+	const chat = {
+		id: '1',
+		user: {
+			image: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg',
+			name: 'Lukas',
+		},
+		lastMessage: {
+			text: 'Hi there. How are you doing. Just wanted to check on theprogress of the project? How is the design coming up?',
+			createdAt: '07:30',
+		},
+	}
+
 	return (
 		<View style={styles.container}>
-			<ChatListItem />
-			<ChatListItem />
+			<ChatListItem chat={chat} />
+			<ChatListItem chat={chat} />
 		</View>
 	)
 }
--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -1,12 +1,12 @@
 import { View, Text, Image, StyleSheet } from 'react-native'
 
-const ChatListItem = () => {
+const ChatListItem = ({ chat }) => {
 	return (
 		<View style={styles.container}>
 			{/* User Avatar */}
 			<Image
 				source={{
-					uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/lukas.jpeg',
+					uri: chat.user.image,
 				}}
 				style={styles.image}
 			/>
@@ -16,14 +16,15 @@ const ChatListItem = () => {
 				{/* Row */}
 				<View style={styles.row}>
 					<Text numberOfLines={1} style={styles.name}>
-						Rellika
+						{chat.user.name}
 					</Text>
-					<Text style={styles.subTitle}>07:30</Text>
+					<Text style={styles.subTitle}>
+						{chat.lastMessage.createdAt}
+					</Text>
 				</View>
 
 				<Text numberOfLines={2} style={styles.subTitle}>
-					Hi there. How are you doing. Just wanted to check on the
-					progress of the project? How is the design coming up?
+					{chat.lastMessage.text}
 				</Text>
 			</View>
 		</View>
