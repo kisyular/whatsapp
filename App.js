825d6387969eaaa5cd7e71066fe5af3b51e730b6
rellika <rellika@lwalacommunityalliance.org>
2022-10-17 00:01:26 +0300

    ADDED NAVIGATOR. CONNECTED IT TO THE CHATLISTITEM.JS

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 // import ChatsScreen from './src/screens/ChatsScreen'
 import ChatScreen from './src/screens/ChatScreen'
+import Navigator from './src/navigation/index'
 
 export default function App() {
 	return (
 		<View style={styles.container}>
 			{/* <ChatsScreen /> */}
-			<ChatScreen />
+			{/* <ChatScreen /> */}
+			<Navigator />
 		</View>
 	)
 }
--- src/components/ChatListItem/index.js
+++ src/components/ChatListItem/index.js
@@ -1,11 +1,22 @@
-import { View, Text, Image, StyleSheet } from 'react-native'
+import { View, Text, Image, StyleSheet, Pressable } from 'react-native'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 dayjs.extend(relativeTime)
+import { useNavigation } from '@react-navigation/native'
 
 const ChatListItem = ({ chat }) => {
+	const navigation = useNavigation()
+
 	return (
-		<View style={styles.container}>
+		<Pressable
+			onPress={() =>
+				navigation.navigate('Chat', {
+					id: chat.id,
+					name: chat.user.name,
+				})
+			}
+			style={styles.container}
+		>
 			{/* User Avatar */}
 			<Image
 				source={{
@@ -30,7 +41,7 @@ const ChatListItem = ({ chat }) => {
 					{chat.lastMessage.text}
 				</Text>
 			</View>
-		</View>
+		</Pressable>
 	)
 }
 
