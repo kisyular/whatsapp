e65e3057e8d61d34451f3e753a2e9a3abfb0e6ab
kisyular <rellika@kisyula.com>
2022-11-11 08:57:50 +0300

    CREATE A CHATROOM, ADD USER TO CHATROOM, DISPLAY CHATS

--- src/components/ContactListItem/index.js
+++ src/components/ContactListItem/index.js
@@ -3,12 +3,57 @@ import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
 dayjs.extend(relativeTime)
 import { useNavigation } from '@react-navigation/native'
+import { Amplify, Auth, API, graphqlOperation } from 'aws-amplify'
+import { createChatRoom, createUserChatRoom } from '../../graphql/mutations'
 
 const ContactListItem = ({ user }) => {
 	const navigation = useNavigation()
 
+	const initiateChat = async () => {
+		console.log('Create a chat room')
+
+		//Create a new chat room
+		const newChatRoomData = await API.graphql(
+			graphqlOperation(createChatRoom, {
+				input: {},
+			})
+		)
+
+		if (!newChatRoomData.data?.createChatRoom) {
+			console.log('Failed to create a chat room')
+			return
+		}
+		const newChatRoom = newChatRoomData?.data.createChatRoom
+
+		//Add clicked 'user' to the chat room
+		await API.graphql(
+			graphqlOperation(createUserChatRoom, {
+				input: {
+					userID: user.id,
+					chatRoomID: newChatRoom.id,
+				},
+			})
+		)
+
+		//Add authenticated user to the chat room
+		const userInfo = await Auth.currentAuthenticatedUser()
+		await API.graphql(
+			graphqlOperation(createUserChatRoom, {
+				input: {
+					userID: userInfo.attributes.sub,
+					chatRoomID: newChatRoom.id,
+				},
+			})
+		)
+
+		navigation.navigate('Chat', {
+			id: newChatRoom.id,
+			name: user.name,
+		})
+	}
+
 	return (
-		<Pressable style={styles.container}>
+		<Pressable onPress={initiateChat} style={styles.container}>
 			{/* User Avatar */}
 			<Image
 				source={{
